1c5d545a5bc70cb615432ac39b3556b0eb729806
gustavo <gustavozurawski@gmail.com>
2022-04-10 20:30:07 -0300

    commit

---
+++ tests/models/test_post.py
@@ -0,0 +1,13 @@
+import pytest
+
+from myblog.blog.factories import PostFactory
+
+
+@pytest.fixture
+def post_publishead():
+    return PostFactory(title='pytest with factory')
+
+
+@pytest.mark.django_db
+def test_create_publishead_post(post_publishead):
+    assert post_publishead.title == 'pytest with factory'
